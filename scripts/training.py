bc13579f5030a09d74fe0003e57270c4960e0341
dcortesav <dcortesav@unal.edu.co>
2026-07-28 23:36:07 -0500

    cleaning up repository and refreshing it with updated project structure focused on training

---
+++ scripts/training.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+from pathlib import Path
+
+def main():
+    # Absolute paths resolved to avoid paths issues
+    ROOT = Path(__file__).resolve().parent.parent
+    DATA = ROOT / "data" / "data.yaml"
+    MODEL = ROOT / "models" / "yolo26x.pt"
+
+    model = YOLO(MODEL)
+
+    '''model.train(
+        data="data/data.yaml",
+        epochs=100,
+        imgsz=640,
+        batch=16,
+        freeze=10,
+        lr0=0.001,
+        lrf=0.01,
+        warmup_epochs=3,
+        mixup=1,
+        cutmix=1,
+        project="training_results",
+        name="v1",
+    )'''
+
+
+if __name__ == "__main__":
+    main()
